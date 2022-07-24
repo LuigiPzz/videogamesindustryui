3ae5433839f6bf95c7e5fdd496ba94c344709ff6
Luigipzz <luigipozzi.it@gmail.com>
2022-07-24 11:45:53 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { VideogameDetailComponent } from './videogame-detail/videogame-detail.co
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { ErrorComponent } from './error/error.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
