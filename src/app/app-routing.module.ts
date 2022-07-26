53d33da004dda1a46a620a0bacd5b1af34820f49
Luigipzz <luigipozzi.it@gmail.com>
2022-07-26 10:20:55 +0200

    Revert "Organizzazione in cartelle"
    This reverts commit bc3c788a03cae390ea6ca2d9bf84fdca45af2c4b.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
-
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { HomeComponent } from './home/home.component';
-import { VideogameComponent } from './videogame/videogame.component';
-import { ConsoleComponent } from './console/console.component';
-import { ProduttoreComponent } from './produttore/produttore.component';
-import { LoginComponent } from '../login/login.component';
-import { LogoutComponent } from '../logout/logout.component';
-import { ErrorComponent } from '../error/error.component';
+import { HomeComponent } from './pages/home/home.component';
+import { VideogameComponent } from './pages/videogame/videogame.component';
+import { ConsoleComponent } from './pages/console/console.component';
+import { ProduttoreComponent } from './pages/produttore/produttore.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { ErrorComponent } from './error/error.component';
 import { RouteGuardServiceService } from 'src/services/route-guard-service.service';
+
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './home/home.component';
-import { VideogameComponent } from './videogame/videogame.component';
-import { ConsoleComponent } from './console/console.component';
-import { ProduttoreComponent } from './produttore/produttore.component';
-import { VideogameDetailComponent } from './videogame-detail/videogame-detail.component';
-import { LoginComponent } from '../login/login.component';
-import { ErrorComponent } from '../error/error.component';
+import { HomeComponent } from './pages/home/home.component';
+import { VideogameComponent } from './pages/videogame/videogame.component';
+import { ConsoleComponent } from './pages/console/console.component';
+import { ProduttoreComponent } from './pages/produttore/produttore.component';
+import { VideogameDetailComponent } from './pages/videogame-detail/videogame-detail.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 import { RouterModule } from '@angular/router';
-import { CoreModule } from '../core/core.module';
+import { CoreModule } from './core/core.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { LogoutComponent } from '../logout/logout.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
